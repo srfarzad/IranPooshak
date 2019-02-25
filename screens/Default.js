dd0a8e16ea72e77de71f200d032ad6505accf1b5
farzad <sr.farzad@gmail.com>
2019-02-25 17:29:46 +0330

    session 4
    call phone
    
    lifecycle react
    
    custom view
    
    add font
    
    slideview

--- screens/Default.js
+++ screens/Default.js
@@ -8,7 +8,7 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, Button, Image,TouchableHighlight,TextInput, Alert} from 'react-native';
+import {Platform, StyleSheet, Text, View, Button, Image, TouchableHighlight, TextInput, Alert} from 'react-native';
 import ProductCategory from "./ProductCategory";
 
 
@@ -20,16 +20,42 @@ const instructions = Platform.select({
 });
 
 type Props = {};
- class Default extends Component<Props> {
 
+class Default extends Component<Props> {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            application: null,
+        }
+
+    }
+
+    getBestProducts() {
+
+        fetch("http://androidsupport.ir/market/getBestApplications.php")
+            .then((response) => response.json())
+            .then((responseJson) => {
+                this.setState({application: responseJson});
+                console.log(responseJson)
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+
+    }
+
+    componentDidMount() {
+        this.getBestProducts();
+    }
 
 
     render() {
         return (
             <View style={styles.container}>
 
-                <Text>  Default page</Text>
-
+                <Text> Default page</Text>
 
 
             </View>
@@ -50,34 +76,34 @@ const styles = StyleSheet.create({
     inputContainer: {
         borderBottomColor: '#F5FCFF',
         backgroundColor: '#FFFFFF',
-        borderRadius:30,
+        borderRadius: 30,
         borderBottomWidth: 1,
-        width:250,
-        height:45,
-        marginBottom:20,
+        width: 250,
+        height: 45,
+        marginBottom: 20,
         flexDirection: 'row',
-        alignItems:'center'
+        alignItems: 'center'
     },
-    inputs:{
-        height:45,
-        marginLeft:16,
+    inputs: {
+        height: 45,
+        marginLeft: 16,
         borderBottomColor: '#FFFFFF',
-        flex:1,
+        flex: 1,
     },
-    inputIcon:{
-        width:30,
-        height:30,
-        marginLeft:15,
+    inputIcon: {
+        width: 30,
+        height: 30,
+        marginLeft: 15,
         justifyContent: 'center'
     },
     buttonContainer: {
-        height:45,
+        height: 45,
         flexDirection: 'row',
         justifyContent: 'center',
         alignItems: 'center',
-        marginBottom:20,
-        width:250,
-        borderRadius:30,
+        marginBottom: 20,
+        width: 250,
+        borderRadius: 30,
     },
     loginButton: {
         backgroundColor: "#00b5ec",
